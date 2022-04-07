d65e3abf114abac983fb717ed00a33484eb49dbd
SudarevAleksandr <39745168+SudarevAleksandr@users.noreply.github.com>
2022-04-08 01:28:14 +0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,107 @@
+import * as React from 'react';
+import { FormControl, InputLabel, Select, MenuItem, Stack } from '@mui/material';
+import { TextField } from '@mui/material';
+import { Button } from '@mui/material';
+import Virtualize from './components/autocompleteReact.js';
+import CustomAutocomplete from './components/CustomAutocomplete.js';
+import './App.css';
+
+async function getDataFromSelltech(type, f){
+  const reqType = {
+    сompanyRelations: 'applicantIndividualCompanyRelations',
+    сompanyPositions: 'applicantIndividualCompanyPositions'
+  };
+  const filter = f ? `(where:{column:NAME, operator:LIKE, value:"${f}"})` : '';
+  let query = `{ ${reqType[type]} ${filter} {data {id name}}}`; 
+  let response = await fetch('https://imperiasexa.ru/api.php?query='+query);
+  response = await response.json();
+  console.log('query', query);
+   return response.data[(reqType[type])].data;
+}
+
+
+
+class App extends React.Component {
+ 
+  constructor(props) {
+      super(props);
+      this.state = {entity: '', сompanyRelations:'' ,сompanyPositions:'' , сompanyRelationsList:[], сompanyPositionsList:[]};
+      this.field = {
+          '':[],
+          'Individual':['firstName','lastName'],
+          'Company':['companyName'],
+      }
+      this.getCompanyRelationsList();
+      this.getCompanyPositionsList();
+  }
+ 
+  getCompanyRelationsList = () => {
+      getDataFromSelltech('сompanyRelations').then(data => {
+        this.setState({сompanyRelationsList: data});
+      });
+  }
+  
+  getCompanyPositionsList = () => {
+      getDataFromSelltech('сompanyPositions', this.state.сompanyRelations).then(data => {
+          this.setState({сompanyPositionsList: data});
+      });
+  }
+ 
+ 
+  onChangeForm = (elem) => {
+      console.log(elem.name, elem.state.value);
+      this.setState({[elem.name]: elem.state.value}, () => {
+        switch(elem.name){
+          case 'сompanyRelations': this.getCompanyPositionsList(); break; 
+          }
+     });
+  }
+ 
+  selectEntity = (e) =>{
+     this.setState({entity: e.target.value});
+  }
+
+  render() {
+    return (
+    <div className="App">
+      <div className='formWrap'> 
+      
+       <Stack spacing={2}>
+          <FormControl fullWidth>
+            <InputLabel id="demo-simple-select-label">Entity</InputLabel>
+            <Select
+              labelId="demo-simple-select-label"
+              id="demo-simple-select"
+              label="Entity"
+              onChange={this.selectEntity}
+              value={this.state.entity}
+            >
+              <MenuItem value={'Individual'}>Individual</MenuItem>
+              <MenuItem value={'Company'}>Company</MenuItem>
+            </Select>
+          </FormControl>
+
+              
+          <Virtualize/>
+          
+          
+          {~this.field[this.state.entity].indexOf('firstName') ? <TextField id="outlined-basic" sx={{ width: 1 }} label="First Name" variant="outlined" /> : null}
+          {~this.field[this.state.entity].indexOf('lastName') ? <TextField id="outlined-basic" sx={{ width: 1 }} label="Last Name" variant="outlined" /> : null}
+          {~this.field[this.state.entity].indexOf('companyName') ? <TextField id="outlined-basic" sx={{ width: 1 }} label="Company Name" variant="outlined" /> : null}
+
+
+          <CustomAutocomplete onChange={this.onChangeForm} name={'сompanyRelations'} data={this.state.сompanyRelationsList} label="Relation to the Company" />
+          <CustomAutocomplete onChange={this.onChangeForm} name={'сompanyPositions'} data={this.state.сompanyPositionsList} label="Position to the Company" />
+          <Stack direction="row-reverse" spacing={2}> 
+              <Button variant="contained">Add</Button>
+              <Button variant="outlined">Cancel</Button>
+          </Stack>
+        </Stack>
+      </div>
+    </div>
+    )
+  }
+}
+
+
+export default App;
---
+++ src/components/CustomAutocomplete.js
@@ -0,0 +1,145 @@
+import * as React from 'react';
+import { styled } from '@mui/material/styles';
+import { Button, TextField, Stack, MenuItem } from '@mui/material';
+
+
+const Listbox = styled('ul')(
+  ({ theme }) => `
+  margin: 2px 0 0;
+  padding: 0;
+  list-style: none;
+  background-color: ${theme.palette.mode === 'dark' ? '#141414' : '#fff'};
+  overflow-y: auto;
+  overflow-x: hidden;
+  max-height: 250px;
+  border-radius: 4px;
+  z-index: 3;
+
+  & li {
+    padding: 5px 12px;
+    display: flex;
+
+    & span {
+      flex-grow: 1;
+    }
+
+    & svg {
+      color: transparent;
+    }
+  }
+
+  & li[aria-selected='true'] {
+    background-color: ${theme.palette.mode === 'dark' ? '#2b2b2b' : '#fafafa'};
+    font-weight: 600;
+
+    & svg {
+      color: #1890ff;
+    }
+  }
+
+  & li[data-focus='true'] {
+    background-color: ${theme.palette.mode === 'dark' ? '#003b57' : '#e6f7ff'};
+    cursor: pointer;
+
+    & svg {
+      color: currentColor;
+    }
+  }
+`,
+);
+
+
+
+
+class CustomAutocomplete extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {focus: false, value:'', searchValue:'', userItem:'', filteredList: this.getFilteredList('')};
+        this.name = this.props.name;
+    } 
+
+    getFilteredList = (val) => { 
+        return val ? this.props.data.filter(item => ~item.name.toLowerCase().indexOf(val.toLowerCase())) : this.props.data;
+    }
+    
+    onChange = (e) => {
+        this.setState({searchValue: e.target.value});
+        this.setState({filteredList: this.getFilteredList(e.target.value)});
+    }
+    
+    changeUserItem  = (e) => {
+       this.setState({userItem: e.target.value});
+    }
+
+    selectItem = (item) => {
+        this.setState({searchValue: item.name, value: item.name, filteredList: this.getFilteredList(item.name)}, () => this.close(true));
+    }
+     
+    deleteItem = (e, item) => {
+        e.stopPropagation();
+        this.props.data.forEach((elem, index, arr) => {
+            if(elem == item) arr.splice(index, 1); 
+        });
+        this.setState({value: '', searchValue: '', filteredList: this.getFilteredList('')}, () => this.props.onChange(this));
+    }
+     
+    addItem = () => {
+        this.setState({value: this.state.userItem, searchValue: this.state.userItem});
+        this.props.data.push({name: this.state.userItem, removable:true, id: new Date().valueOf() });
+        this.setState({filteredList: this.getFilteredList(this.state.userItem), userItem: ''}, () => this.close(true));
+    }
+     
+    open = ()=> {
+      this.setState({focus:true, filteredList: this.getFilteredList(this.state.searchValue)});
+    } 
+     
+    close = (comlete)=> {
+        comlete === true ? this.props.onChange(this) : this.setState({searchValue: this.state.value});
+        this.setState({focus:false});
+        document.removeEventListener( "click" , this.close);
+    }
+
+    render() {
+      return (
+      <div className='CustomAutocomplete'
+          onMouseEnter ={() => document.removeEventListener( "click" , this.close)} 
+          onMouseLeave={() => document.addEventListener("click", this.close)}
+      >   
+            <TextField 
+               label = {this.props.label} 
+               sx = {{ width: 1 }}
+               onFocus = {this.open}
+               onChange={this.onChange}
+               value={this.state.searchValue}
+             />
+              {this.state.focus ? (
+                  <div className='CustomAutocomplete-listWrap'>
+                      <Listbox > 
+                          {this.state.filteredList.map((item) => (
+                              <MenuItem key={item.id} onClick={() => this.selectItem(item)}> 
+                                    {item.name}
+                                    {item.removable ? 
+                                        <button className='deleteButton' onClick={(e) => this.deleteItem(e, item)}>
+                                            <svg focusable="false" aria-hidden="true" viewBox="0 0 24 24" data-testid="CloseIcon">
+                                            <path d="M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12z"></path></svg>
+                                        </button>
+                                    : null}
+                              </MenuItem>
+                          ))}
+                      </Listbox>
+                      
+                      <Stack className='CustomAutocomplete-footer' spacing={2}>
+                          <TextField  sx={{ width: 1 }}  label={this.props.label} variant="outlined" value={this.state.userItem} onChange={this.changeUserItem}/>
+                          <Button variant="contained" onClick={this.addItem}>Add</Button>
+                      </Stack>
+                  </div>
+              ) : null}
+      </div>
+    )
+  }
+}
+
+
+
+export default CustomAutocomplete;
